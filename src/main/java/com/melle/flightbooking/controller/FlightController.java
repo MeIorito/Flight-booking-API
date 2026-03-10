de946c711fb0d9fbfd255730145449d54fbe594f
MeIorito <Mherlaar2004@gmail.com>
2026-03-10 18:08:56 +0100

    Test commit IJ connection

--- src/main/java/com/melle/flightbooking/controller/FlightController.java
+++ src/main/java/com/melle/flightbooking/controller/FlightController.java
@@ -5,6 +5,7 @@ import com.melle.flightbooking.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 public class FlightController {
 
