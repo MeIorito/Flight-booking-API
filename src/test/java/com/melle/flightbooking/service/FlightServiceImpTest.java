cc3a0325df953679ca9a65f36662e5f585b212b2
MeIorito <Mherlaar2004@gmail.com>
2026-03-13 09:31:58 +0100

    findFlightById and getAllFlights unit tests added

--- src/test/java/com/melle/flightbooking/service/FlightServiceImpTest.java
+++ src/test/java/com/melle/flightbooking/service/FlightServiceImpTest.java
@@ -9,6 +9,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -59,5 +62,50 @@ public class FlightServiceImpTest {
 
         assertTrue(result);
         verify(flightRepository).deleteById(id);
+    }
+
+    @Test
+    public void findFlightById_throwsFlightNotFoundException_whenFlightNotFound(){
+        int id = 1;
+
+        when(flightRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(
+                FlightNotFoundException.class,
+                () -> flightServiceImp.getFlightById(id)
+        );
+        verify(flightRepository).findById(id);
+    }
+
+    @Test
+    public void findFlightById_returnsFlight_whenFlightFound(){
+        int id = 1;
+        Flight flight = new Flight();
+        flight.setId(id);
+
+        when(flightRepository.findById(1)).thenReturn(Optional.of(flight));
+
+        Flight result = flightServiceImp.getFlightById(id);
+
+        assertEquals(id, result.getId());
+        verify(flightRepository).findById(id);
+    }
+
+    @Test
+    public void getAllFlights_returnsFlightIterable_whenIterableIsFound(){
+        Flight flight1 = new Flight();
+        flight1.setId(1);
+
+        Flight flight2 = new Flight();
+        flight2.setId(2);
+
+        Iterable<Flight> allFlights = List.of(flight1, flight2);
+
+        when(flightRepository.findAll()).thenReturn(allFlights);
+
+        Iterable<Flight> result = flightServiceImp.getAllFlights();
+
+        assertEquals(2, ((List<Flight>) result).size());
+        verify(flightRepository).findAll();
     }
 }
