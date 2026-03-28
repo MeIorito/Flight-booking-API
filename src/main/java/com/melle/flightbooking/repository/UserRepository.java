4d5544529c53949eb23e739e8664221873d7035e
MeIorito <Mherlaar2004@gmail.com>
2026-03-28 16:34:15 +0100

    All userService functions are now written. Custom repository function also added for checking existing email and finding user by email

--- src/main/java/com/melle/flightbooking/repository/UserRepository.java
+++ src/main/java/com/melle/flightbooking/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+    boolean existsByEmail(String email);
+    User findUserByEmail(String email);
 }
--- src/main/java/com/melle/flightbooking/service/UserServiceImp.java
+++ src/main/java/com/melle/flightbooking/service/UserServiceImp.java
@@ -2,24 +2,63 @@ package com.melle.flightbooking.service;
 
 import com.melle.flightbooking.interfaces.UserService;
 import com.melle.flightbooking.model.User;
+import com.melle.flightbooking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
+@Service
 public class UserServiceImp implements UserService {
+    private final UserRepository userRepository;
 
-    public User register(User newUser){
-
+    @Autowired
+    public UserServiceImp(UserRepository userRepository){
+        this.userRepository = userRepository;
     }
 
-    public User login(String email, String password){
+    public User register(User newUser){
+        boolean isEmailPresent = userRepository.existsByEmail(newUser.getEmail());
 
+        if(isEmailPresent){
+            // Change to custom exception
+            throw new RuntimeException("Email already exists");
+        }
+        return userRepository.save(newUser);
+    }
+
+    // HASHING!!
+    public User login(String email, String password){
+        boolean isEmailPresent = userRepository.existsByEmail(email);
+
+        if(!isEmailPresent){
+            // Change to custom exception
+            throw new RuntimeException("Email does not exist");
+        }
+
+        User user = userRepository.findUserByEmail(email);
+
+        if(!Objects.equals(user.getPassword(), password)){
+            // Change to custom exception
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return user;
     }
 
     public Optional<User> findByEmail(String email){
+        boolean isEmailPresent = userRepository.existsByEmail(email);
 
+        if(!isEmailPresent){
+            // Change to custom exception
+            throw new RuntimeException("Email not found");
+        }
+
+        return Optional.ofNullable(userRepository.findUserByEmail(email));
     }
 
     public Iterable<User> getAllUsers(){
-
+        return userRepository.findAll();
     }
 }
